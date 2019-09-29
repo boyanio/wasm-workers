97f52efd5f330b013d68847d474214cf628b086c
Boyan Mihaylov <hey@boyan.io>
2019-09-29 17:10:53 +0300

    Use `-g` level of debug when compiling to WebAssembly

--- compile-cpp.bat
+++ compile-cpp.bat
@@ -1,3 +1,3 @@
 @echo off
 
-emcc -std=c++11 -g4 -O3 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_colorCells']" -s ERROR_ON_UNDEFINED_SYMBOLS=0 worker.cpp -o build/wasm/worker.wasm
+emcc -std=c++11 -g -O3 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_colorCells']" -s ERROR_ON_UNDEFINED_SYMBOLS=0 worker.cpp -o build/wasm/worker.wasm
