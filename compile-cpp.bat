2afe9c45459da8b18d1573259f80ee32185496bd
Boyan Mihaylov <hey@boyan.io>
2019-09-29 16:13:17 +0300

    Rearrange build parameters in `compile-cpp.bat` to make it easier to read

--- compile-cpp.bat
+++ compile-cpp.bat
@@ -1,3 +1,3 @@
 @echo off
 
-emcc worker.cpp -std=c++11 -g -O3 -o build/wasm/worker.wasm -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_colorCells']" -s ERROR_ON_UNDEFINED_SYMBOLS=0
+emcc -std=c++11 -g4 -O3 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_colorCells']" -s ERROR_ON_UNDEFINED_SYMBOLS=0 worker.cpp -o build/wasm/worker.wasm
